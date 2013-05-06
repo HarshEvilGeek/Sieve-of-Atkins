acbd4e83458f88f1c9850b83c46a07f1277f6481
Akhil Verghese <akhilcherian@gmail.com>
2013-05-07 00:51:55 +0530

    Initial Commit

---
+++ Atkins/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Atkins
+//
+//  Created by Akhil Verghese on 5/7/13.
+//  Copyright (c) 2013 Akhil Verghese. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
