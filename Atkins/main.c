9e61e37456b8e22639c48d5fb96de65da5b80391
Akhil Verghese <akhilcherian@gmail.com>
2013-05-07 13:42:48 +0530

    Put the sieve in a separate function

--- Atkins/main.c
+++ Atkins/main.c
@@ -11,52 +11,44 @@
 #include <time.h>
 #include <math.h>
 
-int main(int argc, const char * argv[])
+long long Atkins_Sieve(char* sieve, long long N, long long square_root)
 {
-    long long N;
-    printf("Enter N: ");
-    scanf("%lld",&N);
-    char *sieve=malloc(N*sizeof(char));
-    for(long long i=0;i<N;i++)
-        sieve[i]=0;
-    int square_root=ceil(sqrt(N));
     sieve[2]=sieve[3]=1;
-    clock_t t_bits=clock();
-    long long fsq,tsq,ysq,n,y,x;
+    long long four_x_square,three_x_square,y_square,n,y,x;
     
     for (x = 1; x <= square_root; x++)
     {
-        fsq=4*x*x;
-        tsq=3*x*x;
+        four_x_square=4*x*x;
+        three_x_square=3*x*x;
         y=1;
-        ysq=1;
-        while((y<=square_root)&(ysq<=N-fsq))
+        y_square=1;
+        while((y<=square_root)&(y_square<=N-four_x_square))
         {
-            n = fsq+ysq;
+            n = four_x_square+y_square;
             if ((n % 12 == 1) | (n % 12 == 5))
                 sieve[n] ^= 1;
             y++;
-            ysq=y*y;
+            y_square=y*y;
         }
         y=1;
-        ysq=1;
-        while((y<=square_root)&(ysq<=N-tsq))
+        y_square=1;
+        while((y<=square_root)&(y_square<=N-three_x_square))
         {
-            n = tsq+ysq;
+            n = three_x_square+y_square;
             if (n % 12 == 7)
                 sieve[n] ^= 1;
             y++;
-            ysq=y*y;
+            y_square=y*y;
         }
         y=1;
-        ysq=1;
-        while((y<x)&(ysq>=(tsq-N)))
+        y_square=1;
+        while(y<x)
         {
-            n = tsq-ysq;
-            if (n % 12 == 11)
+            n = three_x_square-y_square;
+            if ((n<N)&(n % 12 == 11))
                 sieve[n] ^= 1;
             y++;
-            ysq=y*y;
+            y_square=y*y;
         }
     }
     for(long long p=5; p<=square_root;p++)
@@ -68,13 +60,27 @@ int main(int argc, const char * argv[])
                 sieve[i] = 0;
         }
     }
-    t_bits=clock()-t_bits;
     long long noofprimes=0;
     for(long long i=2;i<N;i++)
     {
         if(sieve[i])
             noofprimes++;
     }
+    return noofprimes;
+}
+
+int main(int argc, const char * argv[])
+{
+    long long N,noofprimes;
+    printf("Enter N: ");
+    scanf("%lld",&N);
+    char *sieve=malloc(N*sizeof(char));
+    for(long long i=0;i<N;i++)
+        sieve[i]=0;
+    int square_root=ceil(sqrt(N));
+    clock_t t_bits=clock();
+    noofprimes=Atkins_Sieve(sieve, N, square_root);
+    t_bits=clock()-t_bits;
     printf("Calculated %lld primes in %.2lf",noofprimes,(double)t_bits/CLOCKS_PER_SEC);
     return 0;
 }
